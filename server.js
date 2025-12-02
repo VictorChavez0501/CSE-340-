e40de01a53b2dd2c6b95207420a3d82e05106a6e
VictorChavez0501 <andreus4720.6@gmail.com>
2025-12-02 11:47:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
+ * Primary server file
  *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const env = require("dotenv").config()
+const path = require("path")
 const app = express()
-const static = require("./routes/static")
 
-/* ***********************
+/* ***************
+ * View Engine
+ *****************/
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+/* ***************
+ * Public folder
+ *****************/
+app.use(express.static("public"))
+
+/* ***************
  * Routes
- *************************/
-app.use(static)
+ *****************/
+const staticRoutes = require("./routes/static")
+app.use(staticRoutes)
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+// Home route (required for Assignment 1)
+app.get("/", (req, res) => {
+  res.render("index")   // Render views/index.ejs
+})
+
+/* ***************
+ * Server startup
+ *****************/
 const port = process.env.PORT
 const host = process.env.HOST
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on ${host}:${port}`)
 })
+
