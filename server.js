96e8385f319d433d2041c37c5c7a65ef537fddf6
VictorChavez0501 <andreus4720.6@gmail.com>
2025-12-03 17:24:04 -0600

    Server.js fixed

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
-const homeRoute = require("./routes/home")   // ➜ NUEVO
+const path = require("path")
 
-/* ***********************
- * View Engine
- *************************/
+// View engine
 app.set("view engine", "ejs")
-app.set("views", "./views")
+app.set("views", path.join(__dirname, "views"))
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
-app.use("/", homeRoute)   // ➜ NUEVO: esto sirve el home
+// Static files
+app.use(express.static(path.join(__dirname, "public")))
 
-/* ***********************
- * Port & Host
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
-
-/* ***********************
- * Start Server
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+// Routes
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
