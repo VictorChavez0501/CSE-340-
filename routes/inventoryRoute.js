55c5813ce4b21302af3f087790755ae4b9709051
VictorChavez0501 <andreus4720.6@gmail.com>
2025-12-08 09:03:20 -0600

    Fix error

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/inventoryController");
 
-// Vista de management
+// Rutas principales
 router.get("/", invController.buildManagement);
 
 // Por clasificación
@@ -10,13 +10,5 @@ router.get("/type/:classificationId", invController.buildByClassification);
 
 // Detalle de vehículo
 router.get("/detail/:inventoryId", invController.buildDetailView);
-
-// ADD classification
-router.get("/add-classification", invController.buildAddClassification);
-router.post("/add-classification", invController.addClassification);
-
-// ADD inventory
-router.get("/add-inventory", invController.buildAddInventory);
-router.post("/add-inventory", invController.addInventory);
 
 module.exports = router;
