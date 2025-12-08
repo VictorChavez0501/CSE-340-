bc2bfa27f07d2db7e97da38617d86d1c81457534
VictorChavez0501 <andreus4720.6@gmail.com>
2025-12-08 09:17:14 -0600

    assignment 3 easy

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,15 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
 pool.connect()
-  .then(() => console.log("✅ Conectado a PostgreSQL en Render"))
+  .then(() => console.log("✅ Conectado a PostgreSQL (Render)"))
   .catch(err => console.error("❌ Error conexión PostgreSQL:", err));
 
 module.exports = pool;
